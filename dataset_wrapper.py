847e8d9e8759fec6100a6b495b0ff78611578c5e
mdodovic <matija.dodovic@gmail.com>
2023-12-12 18:08:11 +0100

    remove unnecessary function

--- dataset_wrapper.py
+++ dataset_wrapper.py
@@ -25,21 +25,3 @@ def read_dataset(path):
                 labels.append(label)
 
     return reviews, labels
-
-
-def preprocess_for_bert(reviews, scores):
-    sentiments = []
-    tokenized_reviews = []
-
-    label_encoder = LabelEncoder()
-    encoded_scores = label_encoder.fit_transform([0 if s <= 2 else 1 if s == 3 else 2 for s in scores])
-
-    for review, encoded_score in zip(reviews, encoded_scores):
-        sentiment = "negative" if encoded_score == 0 else "neutral" if encoded_score == 1 else "positive"
-        sentiments.append(sentiment)
-
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        tokens = tokenizer(review, padding=True, truncation=True, return_tensors='pt')
-        tokenized_reviews.append(tokens)
-
-    return sentiments, tokenized_reviews
