281da358d362507e48f0ac818990067b42e2c2d6
mdodovic <matija.dodovic@gmail.com>
2023-12-14 23:04:52 +0100

    callback

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
+from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments, TrainerCallback
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
 
 # Assuming the dataset_wrapper.py has been correctly imported
 from dataset_wrapper import read_dataset
@@ -12,6 +13,38 @@ from dataset_wrapper import read_dataset
 
 device = torch.device("cuda:0" if torch.cuda.device_count() > 0 else "cpu")
 print("Using device:", device)
+
+# Custom callback for training accuracy
+class TrainingAccuracyCallback(TrainerCallback):
+    def __init__(self, train_dataset, batch_size):
+        self.train_dataset = train_dataset
+        self.batch_size = batch_size
+        self.device = device
+
+    def on_step_end(self, args, state, control, **kwargs):
+        if state.global_step % args.logging_steps == 0 and state.global_step > 0:
+            model.eval()  # Set model to evaluation mode
+            total_eval_accuracy = 0
+            total_eval_steps = 0
+
+            for batch in tqdm(DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=False), desc="Accuracy Calculation"):
+                batch = {k: v.to(self.device) for k, v in batch.items()}
+                with torch.no_grad():
+                    outputs = model(**batch)
+
+                logits = outputs.logits
+                preds = torch.argmax(logits, dim=-1)
+                total_eval_accuracy += (preds == batch["labels"]).sum().item()
+                total_eval_steps += batch["labels"].size(0)
+
+            avg_train_accuracy = total_eval_accuracy / total_eval_steps
+            print(f"Training accuracy (step {state.global_step}): {avg_train_accuracy:.4f}")
+
+            # Directly append the accuracy to the trainer's log history
+            state.log_history.append({
+                "step": state.global_step,
+                "train_accuracy": avg_train_accuracy,
+            })
 
 # Custom dataset class for BERT
 class SentimentDataset(Dataset):
@@ -48,7 +81,7 @@ class SentimentDataset(Dataset):
 class FineTuningConfig:
     def __init__(self):
         # Path to Dataset
-        self.dataset_path = 'dataset/finefoods.txt' 
+        self.dataset_path = 'dataset/text.txt' 
         self.labels_num = 3 # Number of labels in the dataset
 
         # Model Training Parameters
@@ -56,16 +89,16 @@ class FineTuningConfig:
         self.train_batch_size = 32 # Batch size for training
         self.eval_batch_size = 32 # Batch size for evaluation
         self.learning_rate = 5e-5 # Learning rate for the optimizer
-        self.warmup_steps = 500 # Number of warmup steps for learning rate scheduler
+        self.warmup_steps = 1 # Number of warmup steps for learning rate scheduler
         self.weight_decay = 0.01 # Regularization parameter
         self.max_seq_length = 512 # Max length of input sequences
 
         # Strategy and steps
         self.log_and_eval_strategy = 'steps'
-        self.log_and_eval_steps = 500  # Choose a value that suits your training regimen
+        self.log_and_eval_steps = 1  # Choose a value that suits your training regimen
         
         self.save_strategy = 'steps'
-        self.save_steps = 500  # Choose a value that suits your training regimen
+        self.save_steps = 1  # Choose a value that suits your training regimen
         self.save_total_limit = 1 
 
         # File Paths and Directories
@@ -108,6 +141,9 @@ test_dataset = SentimentDataset(test_reviews, test_scores, tokenizer, max_len=co
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=config.labels_num)
 model.to(device)
 
+# Instantiate the training accuracy callback
+train_acc_callback = TrainingAccuracyCallback(train_dataset, config.train_batch_size)
+
 # Update the TrainingArguments with the new strategies
 training_args = TrainingArguments(
     output_dir=config.output_dir,
@@ -133,7 +169,8 @@ trainer = Trainer(
     args=training_args,
     train_dataset=train_dataset,
     eval_dataset=val_dataset,
-    compute_metrics=compute_metrics
+    compute_metrics=compute_metrics,
+    callbacks=[train_acc_callback]
 )
 
 # Train the model
@@ -143,42 +180,31 @@ trainer.train()
 test_results = trainer.evaluate(test_dataset)
 print("Test Set Results:", test_results)
 
-# Extracting the training history
-history = trainer.state.log_history
+# Extracting the training history for plotting
+train_loss_set = [log['loss'] for log in trainer.state.log_history if 'loss' in log]
+valid_loss_set = [log['eval_loss'] for log in trainer.state.log_history if 'eval_loss' in log]
+train_accuracy_set = [log['train_accuracy'] for log in trainer.state.log_history if 'train_accuracy' in log]
+valid_accuracy_set = [log['eval_accuracy'] for log in trainer.state.log_history if 'eval_accuracy' in log]
 
-# Initializing lists to store metrics
-train_loss_set = []
-valid_loss_set = []
-train_accuracy_set = []
-valid_accuracy_set = []
+# Plotting training and validation loss and accuracy
+plt.figure(figsize=(12, 6))
 
-# Extracting loss and accuracy for each epoch
-for entry in history:
-    if 'loss' in entry:
-        train_loss_set.append(entry['loss'])
-    elif 'eval_loss' in entry:
-        valid_loss_set.append(entry['eval_loss'])
-    if 'eval_accuracy' in entry:
-        valid_accuracy_set.append(entry['eval_accuracy'])
-
-# Plotting training and validation loss
-plt.figure(figsize=(12, 4))
 plt.subplot(1, 2, 1)
-plt.plot(train_loss_set, label='Training loss')
-plt.plot(valid_loss_set, label='Validation loss')
+plt.plot(train_loss_set, label='Training Loss')
+plt.plot(valid_loss_set, label='Validation Loss')
 plt.title('Training and Validation Loss')
-plt.xlabel('Epochs')
+plt.xlabel('Steps')
 plt.ylabel('Loss')
 plt.legend()
 
-# Plotting training and validation accuracy (if recorded)
-if valid_accuracy_set:
-    plt.subplot(1, 2, 2)
-    plt.plot(valid_accuracy_set, label='Validation accuracy')
-    plt.title('Validation Accuracy')
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.legend()
+plt.subplot(1, 2, 2)
+plt.plot(train_accuracy_set, label='Training Accuracy')
+plt.plot(valid_accuracy_set, label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Steps')
+plt.ylabel('Accuracy')
+plt.legend()
 
-plt.savefig('loss_accuracy.png', dpi=90)
+plt.tight_layout()
+plt.savefig('training_validation_metrics.png', dpi=100)
 plt.show()
